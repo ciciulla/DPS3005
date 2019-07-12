b3344cd5c8dd6569d3c6fd96a9b91d6e8445a079
f.ciciulla@pm.univpm.it <f.ciciulla@pm.univpm.it>
2019-07-12 10:37:29 +0200

    Signed-off-by: user_LAB

---
+++ DPS3005/main.Designer.vb
@@ -0,0 +1,248 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class main
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.cb_port = New System.Windows.Forms.ComboBox()
+        Me.b_connect = New System.Windows.Forms.Button()
+        Me.b_disconnect = New System.Windows.Forms.Button()
+        Me.B_look_on = New System.Windows.Forms.Button()
+        Me.B_look_off = New System.Windows.Forms.Button()
+        Me.b_Tension = New System.Windows.Forms.Button()
+        Me.b_Current = New System.Windows.Forms.Button()
+        Me.Boff = New System.Windows.Forms.Button()
+        Me.Bon = New System.Windows.Forms.Button()
+        Me.v_Tension = New System.Windows.Forms.TextBox()
+        Me.v_Current = New System.Windows.Forms.TextBox()
+        Me.b_info = New System.Windows.Forms.Button()
+        Me.tb_out = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.t_loop = New System.Windows.Forms.Timer(Me.components)
+        Me.cb_loop = New System.Windows.Forms.CheckBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 18)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(26, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Port"
+        '
+        'cb_port
+        '
+        Me.cb_port.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cb_port.FormattingEnabled = True
+        Me.cb_port.Location = New System.Drawing.Point(44, 15)
+        Me.cb_port.Name = "cb_port"
+        Me.cb_port.Size = New System.Drawing.Size(102, 21)
+        Me.cb_port.TabIndex = 1
+        '
+        'b_connect
+        '
+        Me.b_connect.Location = New System.Drawing.Point(166, 11)
+        Me.b_connect.Name = "b_connect"
+        Me.b_connect.Size = New System.Drawing.Size(78, 27)
+        Me.b_connect.TabIndex = 2
+        Me.b_connect.Text = "Connect"
+        Me.b_connect.UseVisualStyleBackColor = True
+        '
+        'b_disconnect
+        '
+        Me.b_disconnect.Location = New System.Drawing.Point(250, 11)
+        Me.b_disconnect.Name = "b_disconnect"
+        Me.b_disconnect.Size = New System.Drawing.Size(78, 27)
+        Me.b_disconnect.TabIndex = 3
+        Me.b_disconnect.Text = "Disconnect"
+        Me.b_disconnect.UseVisualStyleBackColor = True
+        '
+        'B_look_on
+        '
+        Me.B_look_on.Location = New System.Drawing.Point(150, 164)
+        Me.B_look_on.Name = "B_look_on"
+        Me.B_look_on.Size = New System.Drawing.Size(78, 29)
+        Me.B_look_on.TabIndex = 4
+        Me.B_look_on.Text = "Look ON"
+        Me.B_look_on.UseVisualStyleBackColor = True
+        '
+        'B_look_off
+        '
+        Me.B_look_off.Location = New System.Drawing.Point(234, 164)
+        Me.B_look_off.Name = "B_look_off"
+        Me.B_look_off.Size = New System.Drawing.Size(78, 29)
+        Me.B_look_off.TabIndex = 6
+        Me.B_look_off.Text = "Look OFF"
+        Me.B_look_off.UseVisualStyleBackColor = True
+        '
+        'b_Tension
+        '
+        Me.b_Tension.Location = New System.Drawing.Point(250, 67)
+        Me.b_Tension.Name = "b_Tension"
+        Me.b_Tension.Size = New System.Drawing.Size(78, 29)
+        Me.b_Tension.TabIndex = 7
+        Me.b_Tension.Text = "ok"
+        Me.b_Tension.UseVisualStyleBackColor = True
+        '
+        'b_Current
+        '
+        Me.b_Current.Location = New System.Drawing.Point(250, 109)
+        Me.b_Current.Name = "b_Current"
+        Me.b_Current.Size = New System.Drawing.Size(78, 29)
+        Me.b_Current.TabIndex = 8
+        Me.b_Current.Text = "ok"
+        Me.b_Current.UseVisualStyleBackColor = True
+        '
+        'Boff
+        '
+        Me.Boff.Location = New System.Drawing.Point(71, 164)
+        Me.Boff.Name = "Boff"
+        Me.Boff.Size = New System.Drawing.Size(47, 29)
+        Me.Boff.TabIndex = 10
+        Me.Boff.Text = "off"
+        Me.Boff.UseVisualStyleBackColor = True
+        '
+        'Bon
+        '
+        Me.Bon.Location = New System.Drawing.Point(18, 164)
+        Me.Bon.Name = "Bon"
+        Me.Bon.Size = New System.Drawing.Size(47, 29)
+        Me.Bon.TabIndex = 9
+        Me.Bon.Text = "on"
+        Me.Bon.UseVisualStyleBackColor = True
+        '
+        'v_Tension
+        '
+        Me.v_Tension.Location = New System.Drawing.Point(89, 72)
+        Me.v_Tension.Name = "v_Tension"
+        Me.v_Tension.Size = New System.Drawing.Size(145, 20)
+        Me.v_Tension.TabIndex = 11
+        '
+        'v_Current
+        '
+        Me.v_Current.Location = New System.Drawing.Point(89, 114)
+        Me.v_Current.Name = "v_Current"
+        Me.v_Current.Size = New System.Drawing.Size(145, 20)
+        Me.v_Current.TabIndex = 12
+        '
+        'b_info
+        '
+        Me.b_info.Location = New System.Drawing.Point(358, 166)
+        Me.b_info.Name = "b_info"
+        Me.b_info.Size = New System.Drawing.Size(144, 27)
+        Me.b_info.TabIndex = 13
+        Me.b_info.Text = "info"
+        Me.b_info.UseVisualStyleBackColor = True
+        '
+        'tb_out
+        '
+        Me.tb_out.Location = New System.Drawing.Point(358, 11)
+        Me.tb_out.Multiline = True
+        Me.tb_out.Name = "tb_out"
+        Me.tb_out.Size = New System.Drawing.Size(255, 127)
+        Me.tb_out.TabIndex = 14
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 75)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(60, 13)
+        Me.Label2.TabIndex = 15
+        Me.Label2.Text = "Set tension"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 117)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(59, 13)
+        Me.Label3.TabIndex = 16
+        Me.Label3.Text = "Set current"
+        '
+        't_loop
+        '
+        Me.t_loop.Interval = 1000
+        '
+        'cb_loop
+        '
+        Me.cb_loop.AutoSize = True
+        Me.cb_loop.Location = New System.Drawing.Point(532, 172)
+        Me.cb_loop.Name = "cb_loop"
+        Me.cb_loop.Size = New System.Drawing.Size(70, 17)
+        Me.cb_loop.TabIndex = 17
+        Me.cb_loop.Text = "Loop (1s)"
+        Me.cb_loop.UseVisualStyleBackColor = True
+        '
+        'main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(629, 222)
+        Me.Controls.Add(Me.cb_loop)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.tb_out)
+        Me.Controls.Add(Me.b_info)
+        Me.Controls.Add(Me.v_Current)
+        Me.Controls.Add(Me.v_Tension)
+        Me.Controls.Add(Me.Boff)
+        Me.Controls.Add(Me.Bon)
+        Me.Controls.Add(Me.b_Current)
+        Me.Controls.Add(Me.b_Tension)
+        Me.Controls.Add(Me.B_look_off)
+        Me.Controls.Add(Me.B_look_on)
+        Me.Controls.Add(Me.b_disconnect)
+        Me.Controls.Add(Me.b_connect)
+        Me.Controls.Add(Me.cb_port)
+        Me.Controls.Add(Me.Label1)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "main"
+        Me.Text = "Remato contro RD DPS3005"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents cb_port As System.Windows.Forms.ComboBox
+    Friend WithEvents b_connect As System.Windows.Forms.Button
+    Friend WithEvents b_disconnect As System.Windows.Forms.Button
+    Friend WithEvents B_look_on As System.Windows.Forms.Button
+    Friend WithEvents B_look_off As System.Windows.Forms.Button
+    Friend WithEvents b_Tension As System.Windows.Forms.Button
+    Friend WithEvents b_Current As System.Windows.Forms.Button
+    Friend WithEvents Boff As System.Windows.Forms.Button
+    Friend WithEvents Bon As System.Windows.Forms.Button
+    Friend WithEvents v_Tension As System.Windows.Forms.TextBox
+    Friend WithEvents v_Current As System.Windows.Forms.TextBox
+    Friend WithEvents b_info As System.Windows.Forms.Button
+    Friend WithEvents tb_out As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents t_loop As System.Windows.Forms.Timer
+    Friend WithEvents cb_loop As System.Windows.Forms.CheckBox
+
+End Class
---
+++ DPS3005/main.vb
@@ -0,0 +1,140 @@
+﻿Public Class main
+
+    Dim WithEvents tmp_DPS3005 As New DPS3005
+    Dim statePort As Boolean
+    Dim flag_send As Boolean
+    Dim temp_time As Boolean
+
+    Private Sub main_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        tmp_DPS3005.PortListComboBox(cb_port)
+        statePort = False
+        forms_reandaring_port()
+
+    End Sub
+
+
+    Private Sub b_connect_Click(sender As Object, e As EventArgs) Handles b_connect.Click
+        statePort = tmp_DPS3005.Open(cb_port.Text)
+        forms_reandaring_port()
+    End Sub
+
+    Private Sub b_disconnect_Click(sender As Object, e As EventArgs) Handles b_disconnect.Click
+        statePort = tmp_DPS3005.Close()
+        forms_reandaring_port()
+    End Sub
+
+    Sub forms_reandaring_port()
+        If (statePort) Then
+            b_connect.Enabled = False
+            b_disconnect.Enabled = True
+        Else
+            b_connect.Enabled = True
+            b_disconnect.Enabled = False
+        End If
+
+        t_loop.Enabled = Not b_connect.Enabled
+        cb_loop.Checked = Not b_connect.Enabled
+
+    End Sub
+
+
+
+
+    Private Sub B_look_on_Click(sender As Object, e As EventArgs) Handles B_look_on.Click
+        temp_time = t_loop.Enabled
+        Try
+            t_loop.Enabled = False
+            tmp_DPS3005.look_enable()
+            t_loop.Enabled = temp_time
+        Catch ex As Exception
+            MsgBox("Errore input")
+            t_loop.Enabled = temp_time
+        End Try
+    End Sub
+
+    Private Sub B_look_off_Click(sender As Object, e As EventArgs) Handles B_look_off.Click
+        temp_time = t_loop.Enabled
+        Try
+            t_loop.Enabled = False
+            tmp_DPS3005.look_disable()
+            t_loop.Enabled = temp_time
+        Catch ex As Exception
+            MsgBox("Errore input")
+            t_loop.Enabled = temp_time
+        End Try
+    End Sub
+
+    Private Sub Bon_Click(sender As Object, e As EventArgs) Handles Bon.Click
+        temp_time = t_loop.Enabled
+        Try
+            t_loop.Enabled = False
+            tmp_DPS3005.enable()
+            t_loop.Enabled = temp_time
+        Catch ex As Exception
+            MsgBox("Errore input")
+            t_loop.Enabled = temp_time
+        End Try
+    End Sub
+
+    Private Sub Boff_Click(sender As Object, e As EventArgs) Handles Boff.Click
+        temp_time = t_loop.Enabled
+        Try
+            t_loop.Enabled = False
+            tmp_DPS3005.disable()
+            t_loop.Enabled = temp_time
+        Catch ex As Exception
+            MsgBox("Errore input")
+            t_loop.Enabled = temp_time
+        End Try
+    End Sub
+
+    Dim var As Decimal
+    Private Sub b_Tension_Click(sender As Object, e As EventArgs) Handles b_Tension.Click
+        temp_time = t_loop.Enabled
+        Try
+            t_loop.Enabled = False
+            var = Me.v_Tension.Text
+            tmp_DPS3005.set_tension(var)
+            t_loop.Enabled = temp_time
+        Catch ex As Exception
+            MsgBox("Errore input")
+            t_loop.Enabled = temp_time
+        End Try
+    End Sub
+
+    Private Sub b_Current_Click(sender As Object, e As EventArgs) Handles b_Current.Click
+        Try
+            var = Me.v_Current.Text
+            tmp_DPS3005.set_current(var)
+        Catch ex As Exception
+            MsgBox("Errore input")
+        End Try
+
+    End Sub
+
+    Private Sub b_info_Click(sender As Object, e As EventArgs) Handles b_info.Click
+        tmp_DPS3005.get_info()
+    End Sub
+
+    Private Sub t_loop_Tick(sender As Object, e As EventArgs) Handles t_loop.Tick
+        tmp_DPS3005.get_info()
+    End Sub
+    Private Sub cb_loop_CheckedChanged(sender As Object, e As EventArgs) Handles cb_loop.CheckedChanged
+        t_loop.Enabled = cb_loop.Checked
+    End Sub
+
+    Dim tmps As String = ""
+    Private Sub tmp_DPS3005_DataReceived(data As infoDPS3005) Handles tmp_DPS3005.DataReceived
+        tmps = "U-Out " & data.U_Out.ToString & vbCrLf
+        tmps &= "I-Out " & data.I_Out.ToString & vbCrLf
+        tmps &= "P-Out " & data.P_Out.ToString & vbCrLf
+        tmps &= "U-in " & data.I_Out.ToString & vbCrLf
+        tmps &= "Model " & data.Model & vbCrLf
+        tmps &= "Firmware " & data.Firmware & vbCrLf
+
+        Me.Invoke(Sub()
+                      tb_out.Text = tmps
+                  End Sub)
+    End Sub
+End Class
